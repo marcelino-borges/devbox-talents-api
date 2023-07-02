7febba67074f34c28b1970aa26b8aee92b1db47a
Marcelino Borges <botelho_gt@hotmail.com>
2023-07-02 12:03:44 -0300

    removed swagger and unit tests

--- src/config/swagger.ts
+++
@@ -1,8 +0,0 @@
-import { runSwaggerAutogen } from "./../swagger/index";
-import packageJson from "../../package.json";
-
-const updateSwagger = async () => {
-  runSwaggerAutogen(packageJson.version, packageJson.description);
-};
-
-updateSwagger();
